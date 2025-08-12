49d05d507abc169ab8e3f952e926bc9e28e0c939
Santanu Pal <santanu.pal@wipro.com>
2025-08-13 01:07:32 +0530

    remove repeats

---
+++ script.js
@@ -0,0 +1,8 @@
+<script>
+  const hamburger = document.getElementById('hamburger-menu');
+  const navLinks = document.getElementById('nav-links');
+
+  hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+  });
+</script>
